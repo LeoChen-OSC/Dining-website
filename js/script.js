2906658ef4f05ecef52284f6ca8abfea4f61f681
LeoChen-OSC <st25246@ormiston.school.nz>
2026-03-23 11:50:43 +1300

    Created the base website layout

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function show(){
+    document.getElementById("green").classList.toggle("show");
+    //activates the class "show" with the elemend id green
+
+}
+window.onclick = function(event){
+//detects if the user clicks on the window and triggers the function
+    if(!event.target.matches('.toggle-menu')){ //if the user clicks outside of the dropdown button
+        var dropdowns = document.getElementsByClassName("menu-content");
+        //this stores the elemnts in menu-content class in a variable called dropdowns
+        var i;
+        for(i=0; i<dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            //this looks checks if the dropdown menu is open by checking if it has the class "show"
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        //
+        }
+        
+       
+    }
+}
+function redirectlogin(){
+    window.location.href = "login.html";
+}
+function redirectcart(){
+    window.location.href = "cart.html";
+}
+new Swiper("#swiper1",{
+//creates a new swiper using #swiper1
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
